97148769abf6cf942f7e8c697bdf383348843c4f
jaewook1048 <kosil1048@naver.com>
2019-06-18 01:16:20 +0900

    project

--- src/application/Main.java
+++ src/application/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			this.rootStage = primaryStage;
-	        this.rootStage.setTitle("PASSION is Responsibility");
+	        this.rootStage.setTitle("PASSION is Responsibility!");
 	        
 			loadMainBoderPane();
 			loadMemberAnchorPane();			
